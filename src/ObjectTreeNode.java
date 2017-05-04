291cbdbd50129289e92e27799f393b17a7fc8048
Cody Young <cyoung1623@gmail.com>
2017-05-04 13:59:08 -0700

    Added ObjectTreeNode and its interface.
    Signed-off-by: codyayoung <cyoung1623@gmail.com>

--- src/ObjectTreeNode.java
+++ src/ObjectTreeNode.java
@@ -2,4 +2,50 @@
  * Created by masterchef13 on 5/4/17.
  */
 public class ObjectTreeNode {
+    private Object info;
+    private ObjectTreeNode left;
+    private ObjectTreeNode right;
+
+    /**
+     * Constructor for ObjectTreeNode objects.
+     */
+    public ObjectTreeNode() {
+        info = null;
+        left = null;
+        right = null;
+    }
+
+    /**
+     * Overloaded constructor for ObjectTreeNode objects.
+     * @param o
+     */
+    public ObjectTreeNode (Object o) {
+        info = o;
+        left = null;
+        right = null;
+    }
+
+    public void setInfo(Object o) {
+        info = o;
+    }
+
+    public Object getInfo() {
+        return info;
+    }
+
+    public void setLeft(ObjectTreeNode p) {
+        left = p;
+    }
+
+    public ObjectTreeNode getLeft() {
+        return left;
+    }
+
+    public void setRight(ObjectTreeNode p) {
+        right = p;
+    }
+
+    public ObjectTreeNode getRight() {
+        return right;
+    }
 }
