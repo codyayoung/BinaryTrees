53183eb0a2298a78388b3cd361cc2fc4df83e85b
codyayoung <cyoung1623@gmail.com>
2017-05-04 13:51:16 -0700

    Initial commit.

---
+++ src/Comparable.java
@@ -0,0 +1,8 @@
+/**
+ * Interface to allow ObjectList objects to make comparisons.
+ * @author Cody Young
+ * @version 4/11/17
+ */
+public interface Comparable {
+    public int compareTo (Object o);
+}
---
+++ src/ObjectTreeNode.java
@@ -0,0 +1,5 @@
+/**
+ * Created by masterchef13 on 5/4/17.
+ */
+public class ObjectTreeNode {
+}
