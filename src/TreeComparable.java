adee7939952d102006c376a4bad37bbb5e99c302
Cody Young <cyoung1623@gmail.com>
2017-05-08 23:14:01 -0700

    Need to fix casting error in scanning getty.txt.

--- src/TreeComparable.java
+++ src/TreeComparable.java
@@ -1,8 +1,6 @@
 /**
  * Created by masterchef13 on 5/4/17.
  */
-public interface TreeComparable extends ObjectBinaryTreeInterface {
+public interface TreeComparable extends ObjectBinaryTreeInterface, ObjectTreeNodeInterface {
     public int compareTo (Object o);
-    public void visit();
-    public void operate(Object r);
 }
